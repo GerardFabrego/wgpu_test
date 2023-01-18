3849008079dcf015e90bf279305acffab64f5929
Gerard Fabregó <gerardfabrego@Gerards-MacBook-Pro.local>
2023-01-18 15:31:14 +0100

    Init window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+pub async fn run(event_loop: EventLoop<()>, window: Window) {
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                // config.width = size.width;
+                // config.height = size.height;
+                // surface.configure(&device, &config);
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    window.set_title("This is a window!");
+    env_logger::init();
+    pollster::block_on(run(event_loop, window));
 }
