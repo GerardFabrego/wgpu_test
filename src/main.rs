ddf1d16195b90682e78b430d687a557f34d37f02
gerardfabrego <gerardfabrego@gmail.com>
2023-01-19 09:43:02 +0100

    Explore different primitive types

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,31 @@ use std::borrow::Cow;
 use winit::event_loop::EventLoop;
 
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
-    window.set_title("first_triangle");
-    env_logger::init();
+    let mut primitive_type = "triangle-list";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        primitive_type = &args[1];
+    }
+
+    let mut topology = wgpu::PrimitiveTopology::PointList;
+    let mut index_format = None;
+
+    if primitive_type == "triangle-list" {
+        topology = wgpu::PrimitiveTopology::TriangleList;
+        index_format = None;
+    } else if primitive_type == "triangle-strip" {
+        topology = wgpu::PrimitiveTopology::TriangleStrip;
+        index_format = Some(wgpu::IndexFormat::Uint32);
+    }
     let inputs = common::Inputs {
         source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-        topology: wgpu::PrimitiveTopology::TriangleList,
-        strip_index_format: None,
+        topology: topology,
+        strip_index_format: index_format,
     };
-    pollster::block_on(common::run(event_loop, window, inputs, 3));
+
+    let event_loop = EventLoop::new();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    window.set_title(&format!("Primitive {}", primitive_type));
+    env_logger::init();
+    pollster::block_on(common::run(event_loop, window, inputs, 9));
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -5,13 +5,25 @@ struct VertexOutput {
 
 @vertex
 fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {    
-    var pos = array<vec2<f32>,3>(
-        vec2<f32>(0.0,  0.5),
-        vec2<f32>(-0.5,  -0.5),
-        vec2<f32>(0.5, -0.5)
+    var pos = array<vec2<f32>, 9>(
+        vec2<f32>(-0.63, 0.9),
+        vec2<f32>(-0.65, 0.2),
+        vec2<f32>(-0.2, 0.6),
+        vec2<f32>(-0.37, -0.07),
+        vec2<f32>(0.05, 0.18),
+        vec2<f32>(-0.13, -0.4),
+        vec2<f32>(0.3, -0.13),
+        vec2<f32>(0.13, -0.64),
+        vec2<f32>(0.7, -0.3)
     );
 
-    var color = array<vec3<f32>, 3>(
+    var color = array<vec3<f32>, 9>(
+        vec3<f32>(1.0, 0.0, 0.0),
+        vec3<f32>(0.0, 1.0, 0.0),
+        vec3<f32>(0.0, 0.0, 1.0),
+        vec3<f32>(1.0, 0.0, 0.0),
+        vec3<f32>(0.0, 1.0, 0.0),
+        vec3<f32>(0.0, 0.0, 1.0),
         vec3<f32>(1.0, 0.0, 0.0),
         vec3<f32>(0.0, 1.0, 0.0),
         vec3<f32>(0.0, 0.0, 1.0)
