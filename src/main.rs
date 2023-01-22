a178150e69fc779da3ac677f272d1491e0f1b83e
gerardfabrego <gerardfabrego@gmail.com>
2023-01-22 16:39:14 +0000

    Add torus wireframe

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,37 @@ fn create_cone_wireframe(r_up: f32, r_down: f32, height: f32, n: usize) -> Vec<V
     }
     points
 }
+
+fn create_torus_wireframe(r_torus: f32, r_tube: f32, n_torus: u32, n_tube: u32) -> Vec<Vertex> {
+    let mut points: Vec<Vertex> = Vec::with_capacity((4 * n_torus * n_tube) as usize);
+
+    fn torus_position(r_torus: f32, r_tube: f32, u: Deg<f32>, v: Deg<f32>) -> [f32; 3] {
+        let x = (r_torus + r_tube * v.cos()) * u.cos();
+        let y = r_tube * v.sin();
+        let z = -(r_torus + r_tube * v.cos()) * u.sin();
+        [x, y, z]
+    }
+
+    for i in 0..(n_torus - 1) {
+        for j in 0..(n_tube - 1) {
+            let u = i as f32 * 360.0 / (n_torus as f32 - 1.0);
+            let v = j as f32 * 360.0 / (n_tube as f32 - 1.0);
+            let u_diff = (i as f32 + 1.0) * 360.0 / (n_torus as f32 - 1.0);
+            let v_diff = (j as f32 + 1.0) * 360.0 / (n_tube as f32 - 1.0);
+
+            let p0 = torus_position(r_torus, r_tube, Deg(u), Deg(v));
+            let p1 = torus_position(r_torus, r_tube, Deg(u_diff), Deg(v));
+            let p2 = torus_position(r_torus, r_tube, Deg(u), Deg(v_diff));
+
+            points.push(vertex(p0));
+            points.push(vertex(p1));
+            points.push(vertex(p0));
+            points.push(vertex(p2));
+        }
+    }
+    points
+}
 fn main() {
-    let mesh = create_cone_wireframe(0.2, 1.0, 2.5, 20);
+    let mesh = create_torus_wireframe(1.0, 0.4, 30, 20);
     common::run(&mesh);
 }
